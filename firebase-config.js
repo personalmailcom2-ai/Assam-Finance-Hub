054c971653feb9857fec9de07ff5065ae5263b01
personalmailcom2-ai <personalmailcom2@gmail.com>
2026-07-29 10:49:00 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,10 @@
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAnQPG-tdceOqxxfEzHIYdpr6pBJDcOgbM",
+  authDomain: "assam-finance-hub.firebaseapp.com",
+  projectId: "assam-finance-hub",
+  storageBucket: "assam-finance-hub.firebasestorage.app",
+  messagingSenderId: "989678663450",
+  appId: "1:989678663450:web:d0499bf58d8dd382325ea3",
+  measurementId: "G-H1SX3EDQJM"
+};
