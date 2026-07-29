e0cd27ebb23d643c1663a06aa395b96a8ee0c918
personalmailcom2-ai <personalmailcom2@gmail.com>
2026-07-29 15:29:50 +0530

    Update login.js

--- login.js
+++ login.js
@@ -6,6 +6,14 @@ signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
+  apiKey: "AIzaSyAnQPG-tdceOqxxfEzHIYdpr6pBJDcOgbM",
+  authDomain: "assam-finance-hub.firebaseapp.com",
+  projectId: "assam-finance-hub",
+  storageBucket: "assam-finance-hub.firebasestorage.app",
+  messagingSenderId: "989678663450",
+  appId: "1:989678663450:web:d0499bf58d8dd382325ea3",
+  measurementId: "G-H1SX3EDQJM"
+};
   apiKey: "AIzaSyAnQFG-tdce0gxxfEhIYdpr6BJDcQgbW",
   authDomain: "assam-finance-hub.firebaseapp.com",
   projectId: "assam-finance-hub",
