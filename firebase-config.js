1704728a6342dc3287b4d75e3a636c8b736b0353
personalmailcom2-ai <personalmailcom2@gmail.com>
2026-07-29 21:56:33 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAnQFG-tdce0gxxfEhIYdpr6BJDcQgbW",
@@ -15,3 +16,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 window.db = db;
+window.auth = getAuth(app);
