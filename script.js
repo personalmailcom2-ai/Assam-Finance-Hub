edf9d2b1cba4e08a38ffa653d78a14c7d4a95a91
personalmailcom2-ai <personalmailcom2@gmail.com>
2026-07-30 23:22:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -996,3 +996,26 @@ loader.style.display="none";
 
 });
 
+/* Scroll To Top */
+
+const scrollBtn = document.getElementById("scrollTop");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+        scrollBtn.style.display = "flex";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+
+});
+
+scrollBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
+
