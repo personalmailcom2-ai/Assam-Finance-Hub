b39819fc87fdee36e828dc5435147e03b81388c9
personalmailcom2-ai <personalmailcom2@gmail.com>
2026-07-30 12:06:24 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnQPG-tdceOqxxfEzHIYdpr6pBJDcOgbM",
+  authDomain: "assam-finance-hub.firebaseapp.com",
+  projectId: "assam-finance-hub",
+  storageBucket: "assam-finance-hub.firebasestorage.app",
+  messagingSenderId: "989678663450",
+  appId: "1:989678663450:web:d0499bf58d8dd382325ea3",
+  measurementId: "G-H15X3EDQJW"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+const auth = getAuth(app);
+
+export { app, db, auth };
